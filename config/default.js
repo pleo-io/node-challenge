f652fc28de3a15bfe26587420ef7fc9960f81d24
ThankGod Ossaija (Tg) <codebugsolved@gmail.com>
2022-02-22 04:50:32 +0100

    Add config value validations and add .env for managing keys

--- config/default.js
+++ config/default.js
@@ -1,11 +1,23 @@
 require('dotenv').config();
+const Joi = require('joi');
 const path = require('path');
+
+const schema = Joi.object({
+  DB_NAME: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.string().required(),
+  HOST: Joi.string().required(),
+  PORT: Joi.string().required(),
+});
+
+const { error, value } = schema.validate(process.env);
+if (error) throw new Error(`Config validation failed ${error.message}`);
 
 module.exports = {
   db: {
-    host: '0.0.0.0',
-    port: 5432,
-    database: 'challenge',
+    host: value.DB_HOST,
+    port: value.DB_PORT,
+    database: value.DB_NAME,
   },
   debug: {
     stackSize: 4,
@@ -13,11 +25,11 @@ module.exports = {
   i18next: {
     translationFilePath: path.resolve(__dirname, '..', 'locales/{{lng}}/{{ns}}.json'),
   },
-  host: 'localhost:9001',
+  host: value.HOST,
   https: {
     enabled: false,
   },
-  port: process.env.PORT || 9001,
+  port: value.PORT,
   shutdown: {
     appKill: 1000,
     serverClose: 100,
