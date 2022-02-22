8cf9b783ed4b07d8ad56babbb08c6496ea00d923
Pedro Carlos Martinez <mpedroc90@gmail.com>
2022-02-22 18:56:47 +0100

    feat: load configuration based on the environment
    Define configuration agnostic from the code so that we can change it
    it without touching the code.

--- config/default.js
+++ config/default.js
@@ -1,25 +1,31 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const path = require('path');
+
+dotenv.config({
+  path: ['test', 'development', 'qa'].includes(process.env.NODE_ENV) ? path.resolve(__dirname, '..', `.env.${process.env.NODE_ENV}`) : undefined,
+});
 
 module.exports = {
   db: {
-    host: '0.0.0.0',
-    port: 5432,
-    database: 'challenge',
+    host: process.env.DB_HOST || '0.0.0.0',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || 'challenge',
   },
   debug: {
-    stackSize: 4,
+    stackSize: process.env.DEBUG_STACK_SIZE || 4,
   },
   i18next: {
     translationFilePath: path.resolve(__dirname, '..', 'locales/{{lng}}/{{ns}}.json'),
   },
   host: 'localhost:9001',
   https: {
-    enabled: false,
+    enabled: process.env.HTTPS_ENABLED === 'true',
+    key: process.env.HTTPS_KEY,
+    cert: process.env.HTTPS_CERTIFICATE,
   },
   port: process.env.PORT || 9001,
   shutdown: {
-    appKill: 1000,
-    serverClose: 100,
+    appKill: process.env.SHUTDOWN_KILL_IN_MILLISECONDS || 1000,
+    serverClose: process.env.SHUTDOWN_SERVER_CLOSE_IN_MILLISECONDS || 100,
   },
 };
