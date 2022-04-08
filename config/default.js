4cb70f353669559e4c9a671198e1f05715dd288f
Arpit Sharma <sh.arpit30@gmail.com>
2022-04-08 07:35:48 +0530

    sequelize orm supppot

--- config/default.js
+++ config/default.js
@@ -5,6 +5,7 @@ module.exports = {
   db: {
     host: process.env.DB_HOST || '0.0.0.0',
     port: process.env.DB_PORT || 5432,
+    dialect: process.env.DB_DIALECT || 'postgres',
     database: process.env.DB_NAME || 'challenge',
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
