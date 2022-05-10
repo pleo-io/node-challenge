9a34a4656440815bc8211fbb0a1aaf5ba7853e85
Ayman Aladdin <a.alaaeldin@breadfast.com>
2022-05-10 23:15:50 +0200

    refactor validate middleware accept custom errors

--- packages/utils/request-validation.ts
+++ packages/utils/request-validation.ts
@@ -1,30 +1,28 @@
 import { BadRequest } from './errors';
-
 import express from 'express';
 import Joi from 'joi';
 
-export type RequestValidationSchemaType = {
+export type RequestValidationSchemaType = Joi.Schema<{
   query?: Joi.Schema
   body?: Joi.Schema
   params?: Joi.Schema
-}
+}>
 
-export const validate = (schema: Joi.Schema) => {
+export const validate = (schema: RequestValidationSchemaType) => {
   const options = { abortEarly: false, allowUnknown: true, stripUnknown: true };
 
   return (req: express.Request, _: express.Response, next: express.NextFunction) => {
     const data = { body: req.body, params: req.params, query: req.query };
-
-    const { error, value: validatedData } = schema.validate(data, options);
+    const { error, value } = schema.validate(data, options);
 
     if (error) {
-      const message = `Validation error: ${error.details.map((x: Joi.ValidationErrorItem) => x.message).join(', ')}`;
+      const message = error.details?.length ?
+        `Validation error: ${error.details.map((x) => x.message).join(', ')}` : error.message;
 
-      next(BadRequest(message));
+      next(BadRequest(message, req));
     }
 
-    Object.assign(req, validatedData);
-
+    Object.assign(req, value);
     next();
   };
 };
