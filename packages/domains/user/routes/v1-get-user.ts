92076febec335afc2a3af26b5de851779333ffde
Ayman Aladdin <a.alaaeldin@breadfast.com>
2022-05-10 23:18:46 +0200

    refactor get user endpoint and it's response

--- packages/domains/user/routes/v1-get-user.ts
+++ packages/domains/user/routes/v1-get-user.ts
@@ -1,21 +1,22 @@
-import { ApiError } from '@nc/utils/errors';
 import { getUserDetails } from '../model';
+import { getUserValidation } from '../validations/get-user';
 import { Router } from 'express';
-import { secureTrim } from '../formatter';
+import { secureTrimJSON } from '../formatter';
 import { to } from '@nc/utils/async';
+import { validate } from '@nc/utils/request-validation';
+import { ApiError, NotFound } from '@nc/utils/errors';
 
 export const router = Router();
 
-router.get('/get-user-details', async (req, res, next) => {
-  const [userError, userDetails] = await to(getUserDetails(req.query?.userId));
+router.get('/:id', validate(getUserValidation), async (req, res, next) => {
+  const userId = req.params.id;
+  const [userError, userDetails] = await to(getUserDetails(userId));
 
   if (userError) {
     return next(new ApiError(userError, userError.status, `Could not get user details: ${userError}`, userError.title, req));
   }
 
-  if (!userDetails) {
-    return res.json({});
-  }
+  if (!userDetails) return next(NotFound('User, Not found!', req));
 
-  return res.json(secureTrim(userDetails));
+  return res.json(secureTrimJSON(userDetails));
 });
