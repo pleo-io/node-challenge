9db2892a0105d524effc19fe03d26f6808cb74c8
Graham Nash (Nona) <graham.nash@yoco.com>
2022-07-15 17:37:14 +0200

    chore: added docker compose for dev

--- config/dev.js
+++ config/dev.js
@@ -1,1 +1,25 @@
-module.exports = {};
+const path = require('path');
+module.exports = {
+  db: {
+    host: 'db',
+    port: 5432,
+    database: 'challenge',
+    user: 'postgres',
+    password: 'postgres',
+  },
+  debug: {
+    stackSize: 4,
+  },
+  i18next: {
+    translationFilePath: path.resolve(__dirname, '..', 'locales/{{lng}}/{{ns}}.json'),
+  },
+  host: 'localhost:9001',
+  https: {
+    enabled: false,
+  },
+  port: process.env.PORT || 9001,
+  shutdown: {
+    appKill: 1000,
+    serverClose: 100,
+  },
+};
